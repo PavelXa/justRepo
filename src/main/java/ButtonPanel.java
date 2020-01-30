5e4cca930e0f151353b7c49b9f8a719e493f9711
Dvuzhilov Pavel <sciomenihilscires@rambler.ru>
2020-01-30 16:44:03 +0300

    better than it used to be

--- src/main/java/ButtonPanel.java
+++ src/main/java/ButtonPanel.java
@@ -3,17 +3,25 @@ import java.awt.*;
 import java.awt.desktop.ScreenSleepEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class ButtonPanel extends JPanel {
+class ButtonPanel extends JPanel {
+
 
     JTextField screen;
+    private static final char MINUS = '-';
+    private static final char PLUS = '+';
+    private static final char MLTP = '*';
+    private static final char DIV = '/';
 
-    public ButtonPanel() {
-        screen = new JTextField("", 10);
-        screen.setFont(new Font("TimesRoman", Font.BOLD, 50));
-        add(screen, BorderLayout.NORTH);
 
-        setLayout(new GridLayout(5, 4));
+    ButtonPanel() {
+        initSettings();
+        initButtons();
+    }
+
+    private void initButtons() {
+
         JButton jbCancel = new JButton("C");
         jbCancel.setBackground(Color.orange);
         JButton jbRemove = new JButton("«");
@@ -69,116 +77,17 @@ public class ButtonPanel extends JPanel {
         add(jbEQ);
         add(jbDivide);
 
-        jb1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 1);
-                } else {
-                    screen.setText("1");
-                }
-            }
-        });
+        jb1.addActionListener(getListener(jb1.getText()));
+        jb3.addActionListener(getListener(jb3.getText()));
+        jb2.addActionListener(getListener(jb2.getText()));
+        jb4.addActionListener(getListener(jb4.getText()));
+        jb5.addActionListener(getListener(jb5.getText()));
+        jb6.addActionListener(getListener(jb6.getText()));
+        jb7.addActionListener(getListener(jb7.getText()));
+        jb8.addActionListener(getListener(jb8.getText()));
+        jb9.addActionListener(getListener(jb9.getText()));
+        jb0.addActionListener(getListener(jb0.getText()));
 
-        jb2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 2);
-                } else {
-                    screen.setText("2");
-                }
-            }
-        });
-
-        jb3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 3);
-                } else {
-                    screen.setText("3");
-                }
-            }
-        });
-
-        jb4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 4);
-                } else {
-                    screen.setText("4");
-                }
-            }
-        });
-
-        jb5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 5);
-                } else {
-                    screen.setText("5");
-                }
-            }
-        });
-
-        jb6.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 6);
-                } else {
-                    screen.setText("6");
-                }
-            }
-        });
-
-        jb7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 7);
-                } else {
-                    screen.setText("7");
-                }
-            }
-        });
-
-        jb9.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 9);
-                } else {
-                    screen.setText("9");
-                }
-            }
-        });
-
-        jb0.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 0);
-                } else {
-                    screen.setText("0");
-                }
-            }
-        });
-
-        jb8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                if (!screen.getText().equals("0")) {
-                    screen.setText(screen.getText() + 8);
-                } else {
-                    screen.setText("8");
-                }
-            }
-        });
 
         jbCancel.addActionListener(new ActionListener() {
             @Override
@@ -226,9 +135,6 @@ public class ButtonPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String sn = Math.pow(Double.parseDouble(screen.getText()), 2) + "";
-// if (sn.length() > 17) {
-// sn = sn.substring(0, 17);
-// }
                 if (sn.split("\\.")[1].equals("0")) {
                     sn = sn.split("\\.")[0];
                 }
@@ -237,23 +143,140 @@ public class ButtonPanel extends JPanel {
         });
 
         jbMinus.addActionListener(new ActionListener() {
+
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (screen.getText().equals("0")) {
-                    screen.setText("-");
-                }
-
-                if (screen.getText().length() >= 1) {
-
-                    if ((screen.getText().charAt(screen.getText().length() - 1) != '-') | (screen.getText().charAt(screen.getText().length() - 1) != '.')) {
-                        screen.setText(screen.getText() + "-");
+                String text = screen.getText();
+                if (!text.isEmpty()) {
+                    char xh = text.charAt(text.length() - 1);
+                    if (xh == PLUS || xh == MLTP || xh == DIV) {
+                        text = text.substring(0, text.length() - 1);
                     }
-                } else {
-                    screen.setText("-");
+                    if (xh != MINUS) {
+                        screen.setText(text + MINUS);
+                    }
                 }
             }
         });
 
+        jbPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = screen.getText();
+                if (!text.isEmpty()) {
+                    char xh = text.charAt(text.length() - 1);
+                    if (xh == MINUS || xh == MLTP || xh == DIV) {
+                        text = text.substring(0, text.length() - 1);
+                    }
+                    if (xh != PLUS) {
+                        screen.setText(text + PLUS);
+                    }
+                }
+            }
+        });
 
+        jbDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = screen.getText();
+                if (!text.isEmpty()) {
+                    char xh = text.charAt(text.length() - 1);
+                    if (xh == MINUS || xh == MLTP || xh == PLUS) {
+                        text = text.substring(0, text.length() - 1);
+                    }
+                    if (xh != DIV) {
+                        screen.setText(text + DIV);
+                    }
+                }
+            }
+        });
+
+        jbMltp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = screen.getText();
+                if (!text.isEmpty()) {
+                    char xh = text.charAt(text.length() - 1);
+                    if (xh == MINUS || xh == PLUS || xh == DIV) {
+                        text = text.substring(0, text.length() - 1);
+                    }
+                    if (xh != MLTP) {
+                        screen.setText(text + MLTP);
+                    }
+                }
+            }
+        });
+
+        jbEQ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = screen.getText();
+                if (text.contains("*")) {
+
+                }
+            }
+        });
+    }
+
+    private void initSettings() {
+        screen = new JTextField("", 10);
+        screen.setFont(new Font("TimesRoman", Font.BOLD, 50));
+        add(screen, BorderLayout.NORTH);
+        setLayout(new GridLayout(5, 4));
+
+    }
+
+    private ActionListener getListener(String text) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!screen.getText().equals("0")) {
+                    screen.setText(screen.getText() + text);
+                } else {
+                    screen.setText(text);
+                }
+            }
+        };
+    }
+
+    private void count(String text) {
+
+        String element = "";
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != MLTP || text.charAt(i) != DIV || text.charAt(i) != MINUS || text.charAt(i) != PLUS) {
+                element += text.charAt(i);
+            } else {
+                list.add(element);
+                list.add(text.charAt(i) + "");
+                element = "";
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals("*")) {
+                Double itog = Double.parseDouble(list.get(i - 1)) * Double.parseDouble(list.get(i + 1));
+                list.remove(i + 1);
+                list.remove(i);
+                list.set(i - 1, itog + "");
+            }
+            if (list.get(i).equals("/")) {
+                Double itog = Double.parseDouble(list.get(i - 1)) / Double.parseDouble(list.get(i + 1));
+                list.remove(i + 1);
+                list.remove(i);
+                list.set(i - 1, itog + "");
+            }
+        }
+    }
+
+    private ArrayList<String> recursion(ArrayList<String> list) {
+        if (list.contains("*") && list.contains("/")) {
+            return list;
+        } else {
+
+        }
+        return null;
     }
 }
